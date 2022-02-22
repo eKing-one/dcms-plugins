d55a26820a7b43b66a52c6e24f71a239ff2ab941
eKing <228677405@qq.com>
2022-02-22 16:32:42 +0800

    更新文件 count.php

--- Apps/plugins/apps/count.php
+++ Apps/plugins/apps/count.php
@@ -1,6 +1,6 @@
-<?
-$k_p = mysql_result(query("SELECT COUNT(*) FROM `apps`", $db), 0);
-$k_n = mysql_result(query("SELECT COUNT(*) FROM `apps` WHERE `time` > '" . $ftime . "'", $db), 0);
+<?php
+$k_p = dbresult(dbquery("SELECT COUNT(*) FROM `apps`", $db), 0);
+$k_n = dbresult(dbquery("SELECT COUNT(*) FROM `apps` WHERE `time` > '" . $ftime . "'", $db), 0);
 if ($k_n == 0)$k_n = NULL;
 else $k_n = '+' . $k_n;
 echo '(' . $k_p . ') <font color="red">' . $k_n . '</font>';
