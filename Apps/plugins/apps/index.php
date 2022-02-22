28a226a907ce01ceb4a668274700fece94e02e0b
eKing <228677405@qq.com>
2022-02-22 16:32:03 +0800

    更新文件 index.php

--- Apps/plugins/apps/index.php
+++ Apps/plugins/apps/index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 include_once '../../sys/inc/start.php';
 include_once '../../sys/inc/compress.php';
 include_once '../../sys/inc/sess.php';
@@ -10,14 +10,14 @@ include_once '../../sys/inc/fnc.php';
 include_once '../../sys/inc/user.php';
 
 
-if (isset($user) && !isset($_GET['func']) && mysql_result(mysql_query("SELECT COUNT(id_apps) FROM `user_apps`"), 0) > 0) {
+if (isset($user) && !isset($_GET['func']) && dbresult(dbquery("SELECT COUNT(id_apps) FROM `user_apps`"), 0) > 0) {
 	$func = 'user';
 } else {
 	$func = (isset($_GET['func']) ? text($_GET['func']) : 'list');
 }
 
 
-if (is_file('inc/' . $func . '.php')) {
+if (test_file('inc/' . $func . '.php')) {
 	require 'inc/' . $func . '.php';
 } else {
 	header('Location: /index.php');
